15cbcb1e3f3d58b85c1da82b9b5a9d8a4334dc3c
Jose Emilio Antonio Jimenez <a369664@alumnos.uaslp.mx>
2024-10-22 13:06:32 -0600

    Parte 3 agregada

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("boton1").addEventListener("click", redirectFunction1);
+document.getElementById("boton2").addEventListener("click", redirectFunction2);
+
+
+function redirectFunction1() {
+    window.location.href  = "https://www.google.com";
+}
+
+function redirectFunction2() {
+    window.location.href  = "https://www.google.com";
+}
